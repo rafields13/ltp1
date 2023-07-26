bb37f7f60c91f807afa84196593df89aa5c9b3d4
Rafael Marliere de Oliveira <rafael.marliere@dinfor.uniceub>
2023-07-26 20:52:18 -0300

    001 solved

---
+++ aula01/ex001.py
@@ -0,0 +1,3 @@
+# 1. Utilizar nomes de varáveis que fazem sentido com suas funções.
+# 2. Fazer a identação correta do código, para ficar organizado.
+# 3. Deixar comentários os quais expliquem o seu código, para que outros programadores consigam entendê-lo.
