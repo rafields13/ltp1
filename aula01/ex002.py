fbd3823bc05ba7b46a268233faa6983acdbc0c3b
Rafael Marliere de Oliveira <rafael.marliere@dinfor.uniceub>
2023-07-26 21:03:58 -0300

    002 solved

---
+++ aula01/ex002.py
@@ -0,0 +1,5 @@
+# Usamos "def" parra definir a função, logo após colocamos o nome da função, seguida pelos parâmetros que serão utilizados nela. E abrimos a função utilizando os dois pontos (:).
+def soma(a, b): 
+    return a + b # retorno da função
+
+print(soma(5, 2)) # Print para mostrar a função funcionando, somando 5 e 2.
